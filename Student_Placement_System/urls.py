bcfc8cd14b19d2b50659b114b3d0ba8c2638572e
Anncr2003 <161942745+Anncr2003@users.noreply.github.com>
2025-11-26 10:26:11 +0530

    Add files via upload

---
+++ Student_Placement_System/urls.py
@@ -0,0 +1,36 @@
+"""
+URL configuration for Student_Placement_System project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
+from placement_app.views import *
+from placement_app import admin_urls,student_urls
+
+urlpatterns = [
+    path('admin/', admin_urls.urls()),
+    path('student/', student_urls.urls()),
+
+    path('',indexView.as_view(),name='index'),
+    path('login',LoginView.as_view(),name='login'),
+    path('student_reg',StudentReg.as_view(),name='student_reg'),
+
+]
+if settings.DEBUG:
+    urlpatterns+=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
